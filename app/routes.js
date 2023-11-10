cdfeebba9ac87d1d853d024662b9567868cd9d8f
Fernando Jesus Hernandez <fernandoj.hernandez123@gmail.com>
2023-11-10 15:17:11 -0500

    Complete fullstack app (class assignment)

--- app/routes.js
+++ app/routes.js
@@ -1,117 +1,132 @@
-module.exports = function(app, passport, db) {
+module.exports = function (app, passport, db) {
 
-// normal routes ===============================================================
+  // normal routes ===============================================================
 
-    // show the home page (will also have our login links)
-    app.get('/', function(req, res) {
-        res.render('index.ejs');
-    });
+  // show the home page (will also have our login links)
+  app.get('/', function (req, res) {
+    res.render('index.ejs');
+  });
 
-    // PROFILE SECTION =========================
-    app.get('/profile', isLoggedIn, function(req, res) {
-        db.collection('messages').find().toArray((err, result) => {
-          if (err) return console.log(err)
-          res.render('profile.ejs', {
-            user : req.user,
-            messages: result
-          })
-        })
-    });
+  // PROFILE SECTION =========================
+  app.get('/profile', isLoggedIn, function (req, res) {//isLoggedIn is middleware to check if our user is logged in usinf the function at the bottom of this file. this function checks to see if user is logged in and if they arent then they get redirected to main login page. 
+    db.collection('messages').find().toArray((err, result) => {
+      console.log(result)
 
-    // LOGOUT ==============================
-    app.get('/logout', function(req, res) {
-        req.logout(() => {
-          console.log('User has logged out!')
-        });
-        res.redirect('/');
-    });
+      let sortedResult = result.sort((a, b) => (b.thumbUp - b.thumbDown) - (a.thumbUp - a.thumbDown))
+      console.log(sortedResult)
 
-// message board routes ===============================================================
-
-    app.post('/messages', (req, res) => {
-      db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-        if (err) return console.log(err)
-        console.log('saved to database')
-        res.redirect('/profile')
+      if (err) return console.log(err)
+      res.render('profile.ejs', {
+        user: req.user,
+        messages: sortedResult
       })
     })
+  });
 
-    app.put('/messages', (req, res) => {
-      db.collection('messages')
-      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-        $set: {
-          thumbUp:req.body.thumbUp + 1
-        }
+  // LOGOUT ==============================
+  app.get('/logout', function (req, res) {
+    req.logout(() => {
+      console.log('User has logged out!')
+    });
+    res.redirect('/');
+  });
+
+  // message board routes ===============================================================
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  app.post('/messages', (req, res) => {
+    console.log(req.body)
+    db.collection('messages').insertOne({ name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown: 0 }, (err, result) => {//insertOne is inserting the object with the fields that we select from the req.body, which was the info submitted by the user. req.body field names come from ths name attributes of the inputs inside the form element in our index.ejs.
+      if (err) return console.log(err)
+      console.log('saved to database')
+      console.log(result)
+      res.redirect('/profile') //redirects back to main page and uses the app.get request
+    })
+  })
+
+
+  app.put('/messages', (req, res) => {
+    const upOrDown = req.body.hasOwnProperty('thumbDown') ? 'thumbDown' : 'thumbUp'
+    db.collection('messages')
+      .findOneAndUpdate({ name: req.body.name, msg: req.body.msg }, {
+        $inc: { //$sets something from the database and the code below allows us to SET the thumbs up to its value plus one. but I used increment here ($inc) instead. 
+          [upOrDown]: 1,
+        },
       }, {
-        sort: {_id: -1},
+        sort: { _id: -1 },//sorting bottom to top 
         upsert: true
-      }, (err, result) => {
-        if (err) return res.send(err)
-        res.send(result)
-      })
+      },
+        (err, result) => {
+          if (err) return res.send(err)
+          console.log(result)
+          res.send(result)
+        })
+  })
+
+  ////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  app.delete('/messages', (req, res) => {
+    db.collection('messages').findOneAndDelete({ name: req.body.name, msg: req.body.msg }, (err, result) => {
+      if (err) return res.send(500, err)
+      res.send('Message deleted!')
     })
+  })
 
-    app.delete('/messages', (req, res) => {
-      db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
-        if (err) return res.send(500, err)
-        res.send('Message deleted!')
-      })
-    })
+  // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
 
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get('/login', function (req, res) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });//show them login page
+  });
 
-    // locally --------------------------------
-        // LOGIN ===============================
-        // show the login form
-        app.get('/login', function(req, res) {
-            res.render('login.ejs', { message: req.flash('loginMessage') });
-        });
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile', // redirect to the user's secure profile section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
 
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+  // SIGNUP =================================
+  // show the signup form
+  app.get('/signup', function (req, res) {
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
 
-        // SIGNUP =================================
-        // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
-        });
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
 
-        // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
 
-// =============================================================================
-// UNLINK ACCOUNTS =============================================================
-// =============================================================================
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove email and password
-// user account will stay active in case they want to reconnect in the future
-
-    // local -----------------------------------
-    app.get('/unlink/local', isLoggedIn, function(req, res) {
-        var user            = req.user;
-        user.local.email    = undefined;
-        user.local.password = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
+  // local -----------------------------------
+  app.get('/unlink/local', isLoggedIn, function (req, res) {
+    var user = req.user;
+    user.local.email = undefined;
+    user.local.password = undefined;
+    user.save(function (err) {
+      res.redirect('/profile');
     });
+  });
 
 };
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
+  if (req.isAuthenticated())
+    return next();
 
-    res.redirect('/');
+  res.redirect('/');
 }
--- server.js
+++ server.js
@@ -2,29 +2,29 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-const MongoClient = require('mongodb').MongoClient
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+const MongoClient = require('mongodb').MongoClient //setup to help me talk to db
+var mongoose = require('mongoose');//setup mongoose to help us talk to mongodbs better/easier
+var passport = require('passport');//for authentication
+var flash = require('connect-flash');//for our error messages.
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var morgan = require('morgan');//how we are doing our logging in terminal
+var cookieParser = require('cookie-parser');//enables us to look at the logged in session. user can leave computer and still come back can see if theyre still logged in by looking at their cookies.
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
-var configDB = require('./config/database.js');
+var configDB = require('./config/database.js');//grabbing our database same as plugging in server url object
 
 var db
 
 // configuration ===============================================================
-mongoose.connect(configDB.url, (err, database) => {
+mongoose.connect(configDB.url, (err, database) => {//configDB.url is taking the object configDBurl in our config folder's database.js file and getting the url property's value  from that object, which is our server's url.
   if (err) return console.log(err)
   db = database
-  require('./app/routes.js')(app, passport, db);
-}); // connect to our database
+  require('./app/routes.js')(app, passport, db); //running function from routes.js "module.exports = function(app, passport, db) {"
+}); // connect to our mongoDB database
 
 require('./config/passport')(passport); // pass passport for configuration
 
@@ -33,16 +33,16 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static('public'))//setting public folder so we dont have to write individual routes
 
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('view engine', 'ejs'); // set up ejs for templating so we can use the ejs files.
 
 // required for passport
-app.use(session({
-    secret: 'rcbootcamp2021b', // session secret
-    resave: true,
-    saveUninitialized: true
+app.use(session({ //all i need to know about this "required for passport" code is that this creates a new session in my app
+  secret: 'rcbootcamp2021b', // session secret (I can name this whatever I want.)
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
@@ -51,4 +51,4 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 // launch ======================================================================
 app.listen(port);
-console.log('The magic happens on port ' + port);
+console.log('The magic happens on port ' + port);//console.log to show us that our server is running.
